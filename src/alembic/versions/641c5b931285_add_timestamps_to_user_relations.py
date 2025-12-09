b3dbad1934362e02325a9342ebf06f3b051a22d5
Vilius Viskantas <viliusviskantas@gmail.com>
2025-12-09 23:36:35 +0200

    Fix seeding

--- src/alembic/versions/641c5b931285_add_timestamps_to_user_relations.py
+++ src/alembic/versions/641c5b931285_add_timestamps_to_user_relations.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # revision identifiers, used by Alembic.
 revision = 'add_user_relations_timestamps'
-down_revision = 'seed_mock_data'
+down_revision = 'e96f6f7f9f03'
 branch_labels = None
 depends_on = None
 
