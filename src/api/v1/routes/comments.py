b4ad47e1a25a8ed20b768ba2239b6ca46ade582c
Evaldas Leckas <evaldasleckas36@gmail.com>
2025-12-10 20:29:31 +0200

    pridėtas Iq skaičiavimas BE dalyje, naudojant AI API, perduodant komentaro turinį į promtą

--- src/api/v1/routes/comments.py
+++ src/api/v1/routes/comments.py
@@ -138,64 +138,6 @@ def delete(comment_id: int, db: Session = Depends(get_db_session), current_user:
         "message": f"Comment '{comment.content}' deleted successfully"
     }
 
-@comments_router.get("/read/{comment_id}")
-def read(comment_id: int, db: Session = Depends(get_db_session)):
-    comment = db.query(Comment).filter(Comment.id == comment_id).first()
-    if not comment:
-        raise HTTPException(status_code=404, detail="Comment not found")
-
-    return jsonable_encoder({
-        "id": comment.id,
-        "content": comment.content,
-        "created_at": comment.created_at,
-        "modified_at": comment.modified_at,
-        "modified": comment.modified,
-        "author_iq": comment.author_iq,
-        "author_ip_address": comment.author_ip_address,
-    })
-
-@comments_router.put("/update/{comment_id}")
-def update(comment_id: int, comment_data: CommentUpdate, db: Session = Depends(get_db_session), current_user: User = Depends(get_user_by_token)):
-    CURRENT_USER_ID = current_user.id
-    comment = db.query(Comment).filter(Comment.id == comment_id).first()
-    if not comment:
-        raise HTTPException(status_code=404, detail="Comment not found")
-    
-    if comment.author_id != CURRENT_USER_ID:
-        raise HTTPException(status_code=403, detail="Not authorized to delete this comment")
-    if not comment_data.content.strip():
-        raise HTTPException(status_code=400, detail="Comment cannot be empty")
-    comment.content = comment_data.content
-    comment.modified = True
-    comment.modified_at = sa.text("now()")
-
-    db.commit()
-    db.refresh(comment)
-
-    return {
-        "message": "Comment updated successfully",
-        "comment_id": comment.id,
-        "comment_content": comment.content,
-    }
-
-@comments_router.delete("/delete/{comment_id}")
-def delete(comment_id: int, db: Session = Depends(get_db_session), current_user: User = Depends(get_user_by_token)):
-    CURRENT_USER_ID = current_user.id
-    comment = db.query(Comment).filter(Comment.id == comment_id).first()
-    if not comment:
-        raise HTTPException(status_code=404, detail="Comment not found")
-    if comment.author_id != CURRENT_USER_ID:
-        raise HTTPException(status_code=403, detail="Not authorized to delete this comment")
-    
-
-    db.delete(comment)
-    db.commit()
-
-    return {
-        "message": f"Comment '{comment.content}' deleted successfully"
-    }
-
-
 @comments_router.post("/{comment_id}/calculate-iq")
 async def calculate_comment_iq(comment_id: int, req: CommentIQRequest, db: Session = Depends(get_db_session)):
     iq = await calculate_and_save_comment_iq(db, comment_id, req.text)
